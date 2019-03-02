6d7131e50e3a990df1f67dfff3ea74d39e8dbeaf
Tim <timchang.tcc@gmail.com>
2019-03-01 18:32:36 -0800

    Add shadow prop to a posts' Image component

--- src/components/shared/img.tsx
+++ src/components/shared/img.tsx
@@ -4,15 +4,17 @@ import styled from '@emotion/styled'
 type ImageProps = {
   src: string
   alt: string
+  shadow?: boolean
 }
 
-export default function Image({ src, alt }: ImageProps) {
-  return <Img src={src} alt={alt} />
+export default function Image({ src, alt, shadow = false }: ImageProps) {
+  return <Img src={src} alt={alt} shadow={shadow} />
 }
 
 const Img = styled.img`
   width: 100%;
   object-fit: cover;
   border-radius: 3px;
-  box-shadow: var(--base-box-shadow);
+  box-shadow: ${(props: { shadow?: boolean }) =>
+    props.shadow ? 'var(--base-box-shadow)' : 'none'};
 `
